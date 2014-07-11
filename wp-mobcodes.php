3f39c76da662cd22ee5431aae19d80d069a5fbe1
Peter <pitzy@siteup.ro>
2014-07-11 18:05:16 +0300

    functions rename, clean up
    Signed-off-by: Peter <pitzy@siteup.ro>

--- wp-mobcodes.php
+++ wp-mobcodes.php
@@ -1,5 +1,4 @@
 <?php 
-
 	/*
 	Plugin Name: WP MobCodes
 	Version: 1.0.0
@@ -9,7 +8,7 @@
 	Author URI: http://www.siteup.ro
 	License: GPL v3
 
-	WP MobCodes
+	WP Mobile Detect
 	Copyright (C) 2014, Petrisor Buga & Vladi Iancu http://www.siteup.ro
 
 	This program is free software: you can redistribute it and/or modify
@@ -27,8 +26,6 @@
 	*/
 
 
-
-
 	// PHP Mobile Detect class used to detect browser or device type
 	include_once('Mobile_Detect.php');
 	$md = new Mobile_Detect();
@@ -37,44 +34,56 @@
 	$phone = function($atts = array(), $content = "") use ($md) {
 		return ($md->isMobile() && !$md->isTablet) ? (!func_num_args() ? true : do_shortcode($content)) : (!func_num_args() ? false : null); 
 	};
-	function md_is_phone(){ global $phone; return $phone(); }
+	function md_isPhone(){ global $phone; return $phone(); }
 	add_shortcode('phone', $phone );
 
 
 	# display on tablet devices
 	$tablet = function($atts, $content="") use ($md) {
-		return $md->isTablet() ? (!func_num_args() ? true : do_shortcode($content)) : (!func_num_args() ? false : null); 
+		return $md->isTablet() ? 
+				(!func_num_args() ? true : do_shortcode($content)) : 
+				(!func_num_args() ? false : null); 
 	};
-	function md_is_tablet(){ global $tablet; return $tablet(); }
+	function md_isTablet(){ global $tablet; return $tablet(); }
 	add_shortcode('tablet', $tablet);
 
 	# display on phone+tablets
 	$mobile = function($atts, $content="") use ($md) {
-		return ($md->isMobile() || $md->isTablet()) ? (!func_num_args() ? true : do_shortcode($content)) : (!func_num_args() ? false : null); 
+		return ($md->isMobile() || $md->isTablet()) ? 
+				(!func_num_args() ? true : do_shortcode($content)) : 
+				(!func_num_args() ? false : null);
 	};
-	function md_is_mobile(){ global $mobile; return $mobile(); }
+	function md_isMobile(){ global $mobile; return $mobile(); }
 	add_shortcode('mobile', $mobile);
 	add_shortcode('device', $mobile);
 
 	# display only on desktop
 	$desktop = function($atts, $content="") use ($md){
-		return (!$md->isMobile() && !$md->isTablet) ? (!func_num_args() ? true : do_shortcode($content)) : (!func_num_args() ? false : null); 
+		return (!$md->isMobile() && !$md->isTablet) ? 
+				(!func_num_args() ? true : do_shortcode($content)) : 
+				(!func_num_args() ? false : null);
 	};
-	function md_is_desktop(){ global $desktop; return $desktop(); }
+	function md_isDesktop(){ global $desktop; return $desktop(); }
+	function md_isNotDevice(){ return md_isDesktop(); }
+	function md_isNotMobile(){ return md_isDesktop(); }
 	add_shortcode('desktop', $desktop);
 	add_shortcode('notdevice', $desktop); //alias
 	add_shortcode('notmobile', $desktop); //alias
 
 	# only on desktop + tablet
 	$notphone = function($atts, $content="") use ($md) {
-		return (!$md->isMobile() || $md->isTablet()) ? (!func_num_args() ? true : do_shortcode($content)) : (!func_num_args() ? false : null); 
+		return (!$md->isMobile() || $md->isTablet()) ? 
+				(!func_num_args() ? true : do_shortcode($content)) : 
+				(!func_num_args() ? false : null);
 	};
-	function md_is_notphone(){ global $notphone; return $notphone(); }
+	function md_isNotPhone(){ global $notphone; return $notphone(); }
 	add_shortcode('notphone',$notphone);
 
 	# phone + desktop
 	$nottablet = function($atts, $content="") use ($md) {
-		return !$md->isTablet() ? (!func_num_args() ? true : do_shortcode($content)) : (!func_num_args() ? false : null); 
+		return !$md->isTablet() ? 
+				(!func_num_args() ? true : do_shortcode($content)) : 
+				(!func_num_args() ? false : null);
 	};
-	function md_is_nottablet(){ global $nottablet; return $nottablet(); }
+	function md_isNotTablet(){ global $nottablet; return $nottablet(); }
 	add_shortcode('nottablet',$nottablet);
