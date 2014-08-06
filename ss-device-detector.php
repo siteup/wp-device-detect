59ebaf7d3a2070f5dfd15a0b35599e28d86a2f9f
Iancu Vladi <vladi.iancu@siteup.ro>
2014-08-06 12:16:55 +0300

    Updated version display in WP Admin
    Signed-off-by: Iancu Vladi <vladi.iancu@siteup.ro>

--- ss-device-detector.php
+++ ss-device-detector.php
@@ -1,7 +1,7 @@
 <?php 
 	/*
 	Plugin Name: SS Device Detector
-	Version: 1.0.0
+	Version: 1.0.2
 	Plugin URI: http://www.siteup.org.uk/downloads/ss-device-detector-wp-plugin/
 	Description: Server-side solution to detect mobile, tablet and desktop devices. Creates functions and shortcodes
 	Author: SiteUP&trade;
